49cdee8fc018fdcadd8ad5ea20675c102f49b526
A01754887 Ernesto Juárez Torres <a01754887@itesm.mx>
2024-05-23 17:37:40 -0600

    avance

--- src/models/ClienteModel.ts
+++ src/models/ClienteModel.ts
@@ -1,4 +1,4 @@
-import { Model, DataTypes, Sequelize } from 'sequelize';
+import {Model, Sequelize } from 'sequelize';
 
 interface ClienteAttributes {
     clienteId: string;
@@ -6,41 +6,61 @@ interface ClienteAttributes {
     email: string;
     telefono: string;
 }
-
-class Cliente extends Model<ClienteAttributes> implements ClienteAttributes {
-    public clienteId!: string;
-    public nombre!: string;
-    public email!: string;
-    public telefono!: string;
-
-    static associate(models: any) {
+module.exports = (sequelize: Sequelize, DataTypes: any) => {
+    class Cliente extends Model<ClienteAttributes> implements ClienteAttributes {
+        public clienteId!: string;
+        public nombre!: string;
+        public email!: string;
+        public telefono!: string;
+        static associate(models: any) {
         // Asociaciones si son necesarias
+        }
     }
-}
-
-export default (sequelize: Sequelize) => {
     Cliente.init({
         clienteId: {
-            type: DataTypes.STRING,
+            type:DataTypes.STRING,
             allowNull: false,
-            primaryKey: true
+            primaryKey: true,
+            autoIncrement:true
         },
         nombre: {
-            type: DataTypes.STRING,
+            type:DataTypes.STRING,
             allowNull: false
         },
         email: {
-            type: DataTypes.STRING(50),
+            type:DataTypes.STRING,
             allowNull: false
         },
         telefono: {
-            type: DataTypes.STRING(15),
+            type:DataTypes.STRING,
             allowNull: false
         }
     }, {
         sequelize,
         modelName: 'Cliente'
     });
-
+    Cliente.init({
+        clienteId: {
+            type:DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement:true
+        },
+        nombre: {
+            type:DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type:DataTypes.STRING,
+            allowNull: false
+        },
+        telefono: {
+            type:DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+        sequelize,
+        modelName: 'Cliente'
+    });
     return Cliente;
 };
